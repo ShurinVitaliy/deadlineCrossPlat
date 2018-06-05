19f5195ec51a2cbba1e2da05c537e4560ceca240
ShurinVitaliy <34574399+ShurinVitaliy@users.noreply.github.com>
2018-06-05 14:54:51 +0400

    3
    added an admin room

---
+++ easyTask/src/sample/AdminController.java
@@ -0,0 +1,8 @@
+package sample;
+
+/**
+ * Created by User on 05.06.2018.
+ */
+public class AdminController {
+
+}
--- easyTask/src/sample/Controller.java
+++ easyTask/src/sample/Controller.java
@@ -1,15 +1,22 @@
 package sample;
 
+import java.io.IOException;
 import java.net.URL;
 import java.time.Year;
 import java.util.Calendar;
 import java.util.ResourceBundle;
+
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
 import javafx.fxml.FXML;
-import javafx.scene.control.Button;
-import javafx.scene.control.DatePicker;
-import javafx.scene.control.TextArea;
-import javafx.scene.control.TextField;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.*;
 import javafx.scene.image.ImageView;
+import javafx.stage.Stage;
+
+import javax.swing.*;
 
 public class Controller {
 
@@ -42,10 +49,11 @@ public class Controller {
 
     @FXML
     private ImageView imageButtonUser;
+    private int errorMessage;
 
     @FXML
     void initialize() {
-        sendButton.setOnAction(event -> {
+        sendButton.setOnAction((ActionEvent event) -> {
             Calendar calendar = Calendar.getInstance();
             int year = calendar.get(Calendar.YEAR);
             int month = calendar.get(Calendar.MONTH);
@@ -60,6 +68,18 @@ public class Controller {
             if(!passportID.equals("") && !avtoMark.equals("") && !phoneNumber.equals("") && !message.equals("")
                      && !date.equals("")){
                 sendInquiry(passportID, avtoMark, phoneNumber, message, date);
+                passportIDField.setText("");
+                avtoMarkField.setText("");
+                phoneNumberField.setText("");
+                messageField.setText("");
+                messageBox("the request was send!");
+
+            }
+        });
+
+        adminButton.setOnAction(event -> {
+            if(passportIDField.getText().trim().equals("admin")&&avtoMarkField.getText().trim().equals("admin")){
+                openNewScene("/sample/adminRoom.fxml");
             }
         });
 
@@ -69,5 +89,33 @@ public class Controller {
         Inquiry inquiry = new Inquiry(passportID, avtoMark, phoneNumber, date, message);
         DataBase dataBase = new DataBase();
         dataBase.add_inquiry(inquiry);
+    }
+    private void messageBox(String message){
+        Alert alert = new Alert(Alert.AlertType.INFORMATION);
+        alert.setTitle("Message Here...");
+        alert.setHeaderText(message);
+        alert.showAndWait().ifPresent(rs -> {
+            if (rs == ButtonType.OK) {
+                System.out.println("Pressed OK.");
+            }
+        });
+    }
+
+    public void openNewScene(String window){
+        adminButton.getScene().getWindow().hide();
+
+        FXMLLoader loader = new FXMLLoader();
+        loader.setLocation(getClass().getResource(window));
+
+        try {
+            loader.load();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        Parent root = loader.getRoot();
+        Stage stage = new Stage();
+        stage.setScene(new Scene(root));
+        stage.showAndWait();
+
     }
 }
